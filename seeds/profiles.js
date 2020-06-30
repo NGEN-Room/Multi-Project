70dbb955f72780a53ac23b9824e28daf54fbe870
Te Piha Niha <t.niha@ngenroom.co.nz>
2020-06-30 15:00:52 +1200

    added db

---
+++ seeds/profiles.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('profiles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('profiles').insert([
+        {id: 1, 
+        name: 'Rangi Toto',
+        email: 'rangitoto@gmail.com',
+        phone: 021020202020,
+        rate: 50,
+        image: 'images\heeni.png',
+        description: 'Kia ora, Ko Rangitoto toku ingoa',
+        dialect: 'NgaPuhi'
+      }
+      ]);
+    });
+};
